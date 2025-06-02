85310e968ad95c0017e7705328f57eb38c493857
Edivaldo <edivaldo100@gmail.com>
2025-06-02 13:27:14 -0300

    incluir flayway e popular base

--- restapi/src/main/resources/db/migration/V2__insert_initial_data.sql
+++ restapi/src/main/resources/db/migration/V2__insert_initial_data.sql
@@ -33,9 +33,9 @@ INSERT INTO orders (partner_id, total_value, status, created_at, updated_at) VAL
 -- Os IDs dos pedidos serão 1, 2, ..., 10, seguindo a ordem de inserção acima
 INSERT INTO order_items (order_id, product, quantity, unit_price) VALUES
 (1, 'Product A1', 2, 75.00),
-(1, 'Product A2', 1, 0.00), -- Exemplo de valor de item, mas a Order.calculateTotalValue é mais robusta
+(1, 'Product A2', 1, 0.00),
 (2, 'Product B1', 1, 250.00),
-(3, 'Product C1', 3, 25.17), -- 3 * 25.17 = 75.51, arredondado para 75.50 no totalValue
+(3, 'Product C1', 3, 25.17),
 (4, 'Product D1', 1, 300.00),
 (5, 'Product E1', 4, 30.00),
 (6, 'Product F1', 2, 200.00),
